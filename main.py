9f48630bd3e96bf2529c24a9313cc048c0141408
Ruslan Shaiakhmetov <ruslan.shaiakhmetov@unibo.it>
2024-09-08 12:50:28 +0200

    feat: deepcf.dataset_reformulation

--- main.py
+++ main.py
@@ -1,14 +1,91 @@
 #!/usr/bin/env python
 # This line indicates that the script should be run using the Python interpreter.
+
+import sys
+
+# Importing the sys module to manipulate the Python runtime environment.
+
+sys.path.insert(1, "source/")
+# Inserting 'source/' at position 1 in the sys.path list, to allow importing modules from this directory.
+
+import numpy as np
+import multiprocessing
+import matplotlib.pyplot as plt
+from matplotlib import cm
 import source.deepcf as deepcf
 
 # Define a function that prints "Hello, World!"
 def print_hello_world():
-        print("Hello, World!")
+    print("Hello, World!")
+        
+def test():
+    print("Test")
+    params = {
+        "N": 2,
+        "initial_horizon": 2,
+        "prediction_horizon": 3,
+        "n_inputs": 2,
+        "n_outputs": 2,
+    }
+    tot = params["initial_horizon"] + params["prediction_horizon"]
+    obj = deepcf.DeepC_Fragment(params)
+    
+    # Setup the dataset
+    dataset_inputs = []
+    dataset_outputs = []
+    for i in range(0, params["N"]):
+        realization_outputs = []
+        realization_actions = []
+
+        for t in range(0, tot):
+            realization_actions.append(np.array([0.1+t/100+i/1000, -0.1+t/100+i/1000]))
+            realization_outputs.append(np.array([0.2+t/100+i/1000, -0.2+t/100+i/1000]))
+        dataset_inputs.append(np.array(realization_actions).T)
+        dataset_outputs.append(np.array(realization_outputs).T)
+    obj.set_data(dataset_inputs, dataset_outputs)
+    
+    # Setup the initial conditions
+    inputs = np.array([np.array([0.35, -0.35]) for each in range(0, params["initial_horizon"])]).T
+    outputs = np.array([np.array([0.45, -0.45]) for each in range(0, params["initial_horizon"])]).T
+    obj.set_init_cond(inputs, outputs)
+    
+    # Setup the reference
+    inputs = np.array([np.array([0.55, -0.55]) for each in range(0, params["prediction_horizon"])]).T
+    obj.set_reference(inputs)
+    
+    # Setup the criteria
+    criteria = {
+        "Q": [1.0, 1.1],
+        "R": [2.0, 2.1],
+        "lambda_y": [3.0, 3.1],
+        "lambda_g": 4.0,
+    }
+    obj.set_opt_criteria(criteria)
+    
+    # Solve the optimization problem
+    obj.dataset_reformulation(obj.dataset)
+    obj.solve_raw()
+    
+    
         
 # Check if the script is being run as the main module
 if __name__ == "__main__":
+    test()
+    exit()
+    
     # Call the function to print "Hello, World!"
     print_hello_world()
-    deepcf.DeepC_Fragment({})
+    
+    params = {
+        "N": 100,
+        "initial_horizon": 3,
+        "prediction_horizon": 8,
+        "n_inputs": 2,
+        "n_outputs": 4,
+    }
+    
+    obj = deepcf.local_deepc_tracking({})
+    obj.trajectory_tracking()
+    
+    print("End")
     
--- source/deepcf.py
+++ source/deepcf.py
@@ -1,8 +1,110 @@
+import sys
+# Add the path to the sys.path
+sys.path.append("DeePC/source")
+
 import numpy as np
 from scipy import sparse
 import osqp
 import DeePC.source.deepc as deepc
+import DeePC.source.deepc_tracking as deepc_tracking
 
 class DeepC_Fragment(deepc.DeepC):
-    def __init__(self, params: dict):
-        print("Hello from DeepC_Fragment")
+    def dataset_reformulation(self, dataset_in):
+        """
+        Reformulate the dataset for optimization.
+        
+        Inputs:
+        - dataset_in (np.ndarray): Input dataset to be reformulated.
+        
+        Outputs:
+        - None
+        
+        Raises:
+        - Exception: If dataset does not exist.
+        """
+        if not self.dataset_exists:
+            raise Exception("Attempt to reformulate H without dataset")
+        self.H = [[0.0] * (self.init_length * self.channels)] * self.N * self.finish_length
+        for i in range(0, self.N):
+            for ite in range(0, self.finish_length):
+                chunk = np.array([])
+                for j in range(0, self.channels):
+                    chunk = np.hstack((chunk, dataset_in[i][j][ite : self.init_length+ite+1]))
+                self.H[i*self.finish_length+ite] = chunk
+        self.H = np.array(self.H)
+        self.dataset_formulated = True
+    
+    def solve_raw(self):
+        """
+        Solve the optimization problem and provide raw OSQP output.
+        
+        Inputs:
+        - None
+        
+        Outputs:
+        - results: The results from the OSQP solver.
+        
+        Raises:
+        - Exception: If any required data or criteria is missing.
+        """
+        if not self.dataset_formulated:
+            raise Exception("Attempt to solve the problem without formulated dataset")
+        if not self.init_cond_exists:
+            raise Exception("Attempt to solve the problem without initial conditions")
+        if not self.reference_exists:
+            raise Exception("Attempt to solve the problem without reference")
+        if not self.criteria_exists:
+            raise Exception("Attempt to solve the problem without criteria")
+
+        #
+        #
+        #
+        #C1 = zeros(1, finish_length*heg);
+        #for o=0:finish_length-1
+        #    chunk = [u_init, zeros(size(u_init))];
+        #    chunk = circshift(chunk, -o);
+        #    chunk = [chunk(1:init_length), 0];
+
+        #    chunk2 = [y_init, zeros(size(y_init))];
+        #    chunk2 = circshift(y_init, -o);
+        #    chunk2 = [y_init(1:init_length), 0];
+
+        #C1(o*2*(init_length+1)+1:(o+1)*2*(init_length+1)) = [chunk, chunk2];
+
+        #end
+        
+        heg = self.channels * (self.init_length + 1)
+        
+        C1 = np.zeros(self.finish_length * heg)
+        
+        for o in range(0, self.finish_length):
+            chunk = np.concatenate(
+                (
+                    np.concatenate(self.input_init),
+                    np.zeros(self.channels * self.init_length),
+                )
+            )
+            chunk = np.roll(chunk, -o)
+            chunk = chunk[0:self.init_length+1]
+            
+            chunk2 = np.concatenate(
+                (
+                    np.concatenate(self.output_init),
+                    np.zeros(self.channels * self.init_length),
+                )
+            )
+            chunk2 = np.roll(chunk2, -o)
+            chunk2 = chunk2[0:self.init_length+1]
+            
+            C1[o*self.channels*(self.init_length+1):(o+1)*self.channels *
+               (self.init_length+1)] = np.concatenate((chunk, chunk2))
+        
+        
+        print(C1)
+        
+        raise NotImplementedError("Method not implemented")
+        
+class local_deepc_tracking(deepc_tracking.DEEPC_Tracking):
+    
+    def hi(self):
+        print("Hello from DeepC_tracking local")
