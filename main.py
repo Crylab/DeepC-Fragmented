fd796f59ee538082f7d9d8b28b61c2126e23a5f7
Ruslan Shaiakhmetov <ruslan.shaiakhmetov@unibo.it>
2024-10-22 23:50:30 +0200

    feat: LTI

--- main.py
+++ main.py
@@ -11,9 +11,13 @@ sys.path.insert(1, "source/")
 import numpy as np
 import multiprocessing
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.patches as mpatches
 from matplotlib import cm
 import source.deepcf as deepcf
 from source.pendulum import Pendulum_tracking
+from source.linear import LinearSystem, Linear_tracking
+import json
 
 def test_MIMO():
     print("Test")
@@ -267,4 +271,137 @@ def old_main():
 
 # Check if the script is being run as the main module
 if __name__ == "__main__": 
-    print("Main")
+    # Plot the chart
+    error_list = []
+    fig, ax = plt.subplots(1, 2, figsize=(10, 5), width_ratios=[3, 1])
+    for seed in range(0, 100):
+        # Define the parameters for the experiment
+        params = {
+            "Q": [1.0],
+            "R": [0],
+            "lambda_g": 0.5,
+            "lambda_y": [0.5],
+            "algorithm": "deepcf",
+            "prediction_horizon": 10,
+            "seed": seed,
+        }
+        obj = Linear_tracking(params)
+        result = obj.trajectory_tracking()
+        error_list.append(float(obj.error))        
+        ax[0].plot(result)
+
+    ax[0].plot(obj.trajectory.tolist()[5:105], label="Reference", linestyle="--")
+    ax[0].set_xlabel("Time (s)")
+    ax[0].set_ylabel("Displacement $x_2$, (m)")
+    ax[0].set_title("Fragmented DeePC: Linear Tracking")
+    ax[0].legend()
+    ax[0].grid(True)
+    #ax[0].savefig("img/linear_tracking.pdf")
+    #print(error_list)
+
+
+    # Load data from segment.json
+    with open('segment.json', 'r') as file:
+        data = json.load(file)
+
+    # Extract data for the candle plot
+    deepcs_list = data['deepcs']
+    deepcf_list = data['deepcf']
+    deepcs_20_list = data['deepcs_20']
+    deepcf_20_list = data['deepcf_20']
+
+    mean_deepcs = np.mean(deepcs_list)
+    mean_deepcf = np.mean(deepcf_list)
+    mean_deepcs_20 = np.mean(deepcs_20_list)
+    mean_deepcf_20 = np.mean(deepcf_20_list)
+
+    stddeepcs = np.std(deepcs_list)
+    stddeepcf = np.std(deepcf_list)
+    stddeepcs_20 = np.std(deepcs_20_list)
+    stddeepcf_20 = np.std(deepcf_20_list)
+
+    max_deepcs = np.max(deepcs_list)
+    max_deepcf = np.max(deepcf_list)
+    max_deepcs_20 = np.max(deepcs_20_list)
+    max_deepcf_20 = np.max(deepcf_20_list)
+
+    min_deepcs = np.min(deepcs_list)
+    min_deepcf = np.min(deepcf_list)
+    min_deepcs_20 = np.min(deepcs_20_list)
+    min_deepcf_20 = np.min(deepcf_20_list)
+
+    # Print the max values
+    print("Max DeepCS: ", max_deepcs)
+    print("Max DeepCF: ", max_deepcf)
+    print("Max DeepCS 20: ", max_deepcs_20)
+    print("Max DeepCF 20: ", max_deepcf_20)
+
+    # Print the min values
+    print("Min DeepCS: ", min_deepcs)
+    print("Min DeepCF: ", min_deepcf)
+    print("Min DeepCS 20: ", min_deepcs_20)
+    print("Min DeepCF 20: ", min_deepcf_20)
+
+    # Print the mean values
+    print("Mean DeepCS: ", mean_deepcs)
+    print("Mean DeepCF: ", mean_deepcf)
+    print("Mean DeepCS 20: ", mean_deepcs_20)
+    print("Mean DeepCF 20: ", mean_deepcf_20)
+
+    # Print the standard deviation values
+    print("Std DeepCS: ", stddeepcs)
+    print("Std DeepCF: ", stddeepcf)
+    print("Std DeepCS 20: ", stddeepcs_20)
+    print("Std DeepCF 20: ", stddeepcf_20)
+
+    # Plot the candle plot
+
+    
+
+    #fig, ax = plt.subplots()
+
+    # Create a list of labels for the x-axis
+    #labels = ['Fragmented\n$T_{fut}$=10', 'Segmented\n$T_{fut}$=10', 'Fragmented\n$T_{fut}$=20', 'Segmented\n$T_{fut}$=20']
+    labels = ['$\;\;\;\;\;\;\;\;\;\;T_{fut}$=10', '', '$\;\;\;\;\;\;\;\;\;\;T_{fut}$=20']
+
+    # Create a list of means and standard deviations
+    means = [mean_deepcf, mean_deepcs, mean_deepcf_20, mean_deepcs_20]
+    stds  = [stddeepcf,  stddeepcs,  stddeepcf_20,  stddeepcs_20]
+    mins  = [min_deepcf, min_deepcs, min_deepcf_20, min_deepcs_20]
+    maxs  = [max_deepcf, max_deepcs, max_deepcf_20, max_deepcs_20]
+
+    # Create the candlestick plot
+    for i in range(4):
+        ax[1].plot([i, i], [mins[i], maxs[i]], color='black')
+        ax[1].plot([i - 0.1, i + 0.1], [mins[i], mins[i]], color='black')
+        ax[1].plot([i - 0.1, i + 0.1], [maxs[i], maxs[i]], color='black')
+        ax[1].plot([i - 0.2, i + 0.2], [means[i], means[i]], color='red')
+        ax[1].add_patch(plt.Rectangle((i - 0.4, means[i] - stds[i]), 0.8, 2 * stds[i], 
+                                    color = 'blue' if i%2 == 0 else 'orange', alpha=0.3))
+
+    # Set the x-axis labels
+    ax[1].set_xticks(range(len(labels)))
+    ax[1].set_xticklabels(labels)
+
+    # Set the y-axis label
+    ax[1].set_ylabel('Sum of set-point errors')
+
+    # Set the x-axis label
+    ax[1].set_xlabel('Prediction horizon')
+
+    # Set the title
+    ax[1].set_title('DeePC: Fragmented vs. Segmented')
+
+    red_patch = mpatches.Patch(color='blue', label='Fragmented', alpha=0.3)
+    blue_patch = mpatches.Patch(color='orange', label='Segmented', alpha=0.3)
+
+    ax[1].legend(handles=[red_patch, blue_patch])
+
+    ax[1].grid(True)
+
+    # Save the plot
+    plt.savefig("img/candlestick_plot.pdf")
+
+    # Show the plot
+    plt.show()
+
--- source/linear.py
+++ source/linear.py
@@ -44,7 +44,7 @@ class LinearSystem:
             input = -self.parameters["max_input"]
         
         # Noise generation
-        measurement_noise = np.random.normal(0.0, self.parameters["measurement_std_dev"])
+        measurement_noise = 0.0#np.random.normal(0.0, self.parameters["measurement_std_dev"])
 
         sinusoidal_noise = np.sin(2*np.pi*self.parameters["sinusoid_freq"]*self.time) * self.parameters["sinusoid_amplitude"] + self.parameters["sinusoid_bias"]
 
@@ -100,17 +100,20 @@ class Linear_tracking:
         
         self.parameters = {}
         
-        self.set_default_parameters(parameters, "N", 20)
+        
         self.set_default_parameters(parameters, "R", [1.0])
         self.set_default_parameters(parameters, "Q", [1.0])
         self.set_default_parameters(parameters, "lambda_y", [1])
-        self.set_default_parameters(parameters, "lambda_g", 1.0)
+        self.set_default_parameters(parameters, "lambda_g", 0.5)
         self.set_default_parameters(parameters, 'initial_horizon', 5)
         self.set_default_parameters(parameters, 'prediction_horizon', 10)
         self.set_default_parameters(parameters, 'dt', 1.0)
         self.set_default_parameters(parameters, 'algorithm', "deepcf")
         self.set_default_parameters(parameters, 'seed', 1)
         self.set_default_parameters(parameters, 'tracking_time', 100)
+
+        dataset_size = 300 - self.parameters['initial_horizon'] - self.parameters['prediction_horizon']
+        self.parameters['N'] = dataset_size
         
         # Horizon parameters
         self.INITIAL_HORIZON = self.parameters['initial_horizon']
@@ -136,40 +139,34 @@ class Linear_tracking:
         else:
             raise ValueError("Invalid algorithm")
         
-        self.set_random_trajectory(self.parameters['tracking_time'])
+        meander = np.array([0.4]*(25+self.INITIAL_HORIZON) + [-0.24]*20 + [-0.08]*25 + [-0.4]*20 + [0.4]*(10+self.PREDICTION_HORIZON))
+
+        self.set_trajectory(meander)
         self.solver.set_opt_criteria(self.parameters.copy())
-        
-    def __trajectory_generation(self, x: float, x_d: float, l: int):
-        
-        self.model.Initialization(x, x_d)
-        max_input = self.parameters["max_input"]
-        tau = 2 * max_input * np.random.random(l) - max_input
-        out = np.zeros(l)
-        out[0] = x
-        for t in range(1, l):
-            x_p = self.model.Step(tau[t])
-            out[t] = x_p
-        return tau, out
-        
+                
     def dataset_generation(self) -> None:
         dataset_inputs = []
         dataset_outputs = []
-        for i in range(0, self.parameters["N"]):    
-            total_length = self.parameters["initial_horizon"] + self.parameters["prediction_horizon"]
-            init_x = (2*np.random.random()-1) * np.pi
-            init_x_d = (2*np.random.random()-1) * np.pi
-            tau, out = self.__trajectory_generation(init_x, init_x_d, total_length)
-            dataset_inputs.append([tau])
-            dataset_outputs.append([out])
+        self.model.Initialization()
+        max_input = self.parameters["max_input"]
+        tau = []
+        out = []
+        for i in range(30):
+            val = 4 * max_input * np.random.random() - (2 * max_input)
+            for _ in range(10):
+                tau.append(val)
+        
+        for i in range(300):
+            out.append(self.model.Step(tau[i]))
+
+        total_length = self.parameters["initial_horizon"] + self.parameters["prediction_horizon"]
+        for i in range(300-total_length):
+            dataset_inputs.append([tau[i:i+total_length]])
+            dataset_outputs.append([out[i:i+total_length]])
         self.solver.set_data(dataset_inputs, dataset_outputs)
-        
-    def set_random_trajectory(self, l: int) -> None:
-        _, self.trajectory = self.__trajectory_generation(
-            (2*np.random.random()-1) * np.pi, 
-            (2*np.random.random()-1) * np.pi, l)
-        
+                
     def set_trajectory(self, trajectory: np.ndarray) -> None:
-        if trajectory.size == self.parameters['tracking_time']:
+        if trajectory.size == self.parameters['tracking_time']+self.INITIAL_HORIZON+self.PREDICTION_HORIZON:
             self.trajectory = trajectory
         else:
             raise ValueError("Invalid trajectory size")
@@ -206,6 +203,14 @@ class Linear_tracking:
             dist = np.abs(result[i]-self.trajectory[i+shift])
             bank += dist
         return bank/len(result)
+    
+    def __abs_tracking_error(self, result: list) -> float:
+        bank = 0.0
+        shift = self.INITIAL_HORIZON + 1
+        for i in range(0, len(result)):
+            dist = np.abs(result[i]-self.trajectory[i+shift])
+            bank += dist
+        return bank
         
     def __save_result(self, result: list, error: float, exec_time: float) -> None:
         pass
@@ -213,8 +218,6 @@ class Linear_tracking:
     def trajectory_tracking(self):
                 
         # Check if simulation already exists
-        
-        
         self.past_states = self.trajectory[:self.INITIAL_HORIZON].copy().tolist()
         for _ in range(self.INITIAL_HORIZON):
             self.past_actions.append(0.0)
@@ -224,7 +227,7 @@ class Linear_tracking:
         self.dataset_generation()
         self.solver.dataset_reformulation(self.solver.dataset)
                 
-        self.model.Initialization(x=self.past_states[-1])    
+        self.model.Initialization()    
                 
         result = []
         
@@ -255,11 +258,14 @@ class Linear_tracking:
         exec_time = time.time()-start_time
         
         error_per = self.__tracking_error(result)
+
+        self.error = self.__abs_tracking_error(result)
         
         self.rss = error_per
                     
         print(colored('Trajectory tracking: Succeded', 'green'))
         print(colored('Accumulated tracking error per point:', 'green'), colored('{:.2f} m.'.format(error_per), 'white'))
+        print(colored('Accumulated tracking error:', 'green'), colored('{:.2f} m.'.format(self.error), 'white'))
         print(colored('Execution time:', 'green'), colored('{:.2f} s.'.format(exec_time), 'white'))
         print(colored('Averege responce time:', 'green'), colored('{:.2f} s.'.format(exec_time/total_time), 'white'))
                         
